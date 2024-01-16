df88f22cc969a2f38e912a392c9b5e9347f25546
Lionorwot <156789248+Lionorwot@users.noreply.github.com>
2024-01-16 17:10:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94763036132', 'me', true],
+  ['94763036132', 'Nizzu', true], 
   [''] 
 ] //Number of owners
 
